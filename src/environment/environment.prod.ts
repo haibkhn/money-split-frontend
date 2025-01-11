52c300c2bcdab3beb995f102bacd2255ab56690d
haibkhn <hainguyen2000.bkhn@gmail.com>
2025-01-11 17:00:51 +0100

    update build prod command

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://money-split-backend-production.up.railway.app/',
+  apiUrl: 'https://money-split-backend-production.up.railway.app',
 };
